c5f0c92230c771680a8e94b4b5a9708ef03d8892
Val435 <ia.valentina@ufg.edu.sv>
2024-02-20 19:56:53 -0600

    Actividad

---
+++ app/src/main/java/sv/edu/ufg/fis/amb/pocasangrebonilla_pb100221/ActivitySecundaria.kt
@@ -0,0 +1,20 @@
+package sv.edu.ufg.fis.amb.pocasangrebonilla_pb100221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ActivitySecundaria : AppCompatActivity() {
+
+    private lateinit var botonAtras:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secundaria)
+
+        botonAtras = findViewById(R.id.btn_volver)
+
+        botonAtras.setOnClickListener{
+            finish()
+        }
+    }
+}
--- app/src/main/java/sv/edu/ufg/fis/amb/pocasangrebonilla_pb100221/MainActivity.kt
+++ app/src/main/java/sv/edu/ufg/fis/amb/pocasangrebonilla_pb100221/MainActivity.kt
@@ -2,10 +2,20 @@ package sv.edu.ufg.fis.amb.pocasangrebonilla_pb100221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEjecutar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEjecutar = findViewById(R.id.btn_disparador)
+        btnEjecutar.setOnClickListener{
+                val intent = Intent(this,ActivitySecundaria::class.java)
+                startActivity(intent)
+        }
     }
 }
